c89767c030cd1c6611b3736a2b75cf6bb3f64244
Patrick Nicezi <patrickk0806@gmail.com>
2025-09-28 01:45:36 -0300

    chore: disable context loads test

--- src/test/java/br/com/geeknizado/financial_management/FinancialManagementApplicationTests.java
+++ src/test/java/br/com/geeknizado/financial_management/FinancialManagementApplicationTests.java
@@ -1,8 +1,10 @@
 package br.com.geeknizado.financial_management;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class FinancialManagementApplicationTests {
 
